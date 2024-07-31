58f36bd2ade3805ec43ed648268d6c7cfc3b82f0
memesmith1 <115422357+memesmith1@users.noreply.github.com>
2024-07-31 07:43:38 -0700

    Update krivine.py
    solved first lambda still need to do the success one

--- krivine.py
+++ krivine.py
@@ -6,30 +6,40 @@ class krivine_machine:
     environment = []
     #apply a to b
     def apply (self):
-          
-        self.stack = [ [self.term[ -1 ] , self.environment ] ,
-            self.stack
-            ]
-            
-        self.term = self.term[ -2 ]
+
+        print("apply\n")  
+        self.stack = [self.term[ -1 ] , self.environment ]
+
+        if type(self.term[-2]) == int:
+            self.term=[self.term[-2]]
+        else:
+            self.term = self.term[ -2 ]
             
         #self.environment unchanged
 
         #perform lambda abstraction
     def abstract (self):
+        print("abstract\n")
           
-        self.environment = [ self.stack[ 0 ] , self.environment ]
+        self.environment = self.stack
          
-        self.stack = self.stack[ 1 ]
+        self.stack = []
             
-        self.term = self.term[ -1 ]
+        self.term = self.term[1:]
 
          #zero instruction
     def zero (self):
 
-        self.term = self.environment[ 0 ][ 0 ]
+        print("zero\n")
+#        print("environment:\n")
+ #       print(self.
+
+        if type(self.environment[0]) == int:
+            self.term = [self.environment[ 0 ]]
+        else: 
+            self.term = self.environment[ 0 ]
             
-        self.environment =  self.environment[ 0 ][ 1 ]
+        self.environment =  self.environment[ 1 ]
 
         #self.stack unchanged
 
@@ -37,21 +47,26 @@ class krivine_machine:
 
     def success (self):
 
+        print("success\n")
+
         self.term[ -1 ] = self.term[ -1 ] - 1
 
         self.environment = self.environment[ 1 ]
 
     def choose(self):
-        if type(self.term[-1]) == int:
+        print(self.term)
+        print(self.stack)
+        print(self.environment)
+        if self.term[0] == -1:
+            self.abstract(self)
+        elif len(self.term) == 1:
             if self.term[-1] == 0 :
                 self.zero(self)
             else : 
                 self.success(self)
-        elif self.term[-2] == -1:
-            self.abstract(self)
         else :
             self.apply(self)
-        print(self.term)
+
             
     def run(self, input_lambda):
         self.stack = []
@@ -59,11 +74,12 @@ class krivine_machine:
         self.term = input_lambda
         
         self.choose(self);
-        while (len(self.stack) and len(self.environment)):
+        while (len(self.stack) or len(self.environment)):
             self.choose(self);
-        return self.stack
+            
+        return self.term
 
-krivine_machine.run(krivine_machine, [[-1,0,0],[-1,0]])
+print(krivine_machine.run(krivine_machine, [[-1,0,0],[-1,0]]))
 
         
      
