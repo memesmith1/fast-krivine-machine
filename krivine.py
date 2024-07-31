869c5236a99ab1f9c614cf1b5e4e737eadc45cb2
memesmith1 <115422357+memesmith1@users.noreply.github.com>
2024-07-31 05:53:58 -0700

    Update krivine.py
    updated indentation tried to fix syntax error on line approximately 49 but failed

--- krivine.py
+++ krivine.py
@@ -1,64 +1,66 @@
 #fix indenting of match
 #https://emacs.stackexchange.com/questions/69291/how-can-i-make-python-mode-correctly-indent-pythons-match-statement
 class krivine_machine:
-      term = []
-      stack = []
-      environment = []
-      #apply a to b
-      def apply ():
+    term = []
+    stack = []
+    environment = []
+    #apply a to b
+    def apply (self):
+          
+        self.stack = [ [term[ -1 ] , self.environment ] ,
+            self.stack
+            ]
             
-            self.stack = [ [term[ -1 ] , self.environment ] ,
-                           self.stack
-                          ]
+        self.term = term[ -2 ]
             
-            self.term = term[ -2 ]
+        #self.environment unchanged
+
+        #perform lambda abstraction
+    def abstract (self):
+          
+        self.environment = [ self.stack[ 0 ] , self.environment ]
+         
+        self.stack = self.stack[ 1 ]
             
-            #self.environment unchanged
+        self.term = self.term[ -1 ]
 
-      #perform lambda abstraction
-      def abstract ():
+         #zero instruction
+    def zero (self):
+
+        self.term = self.environment[ 0 ][ 0 ]
             
-            self.environment = [ self.stack[ 0 ] , self.environment ]
-            
-            self.stack = self.stack[ 1 ]
-            
-            self.term = self.term[ -1 ]
+        self.environment =  self.environment[ 0 ][ 1 ]
 
-      #zero instruction
-      def zero ():
+        #self.stack unchanged
 
-            self.term = self.environment[ 0 ][ 0 ]
-            
-            self.environment =  self.environment[ 0 ][ 1 ]
+        #succ instruction
 
-            #self.stack unchanged
+    def success (self):
 
-      #succ instruction
-      def success ():
+        self.term = self.term - 1
 
-            self.term = self.term - 1
+        self.environment = self.environment[ 1 ]
 
-            self.environment = self.environment[ 1 ]
+        #self.stack unchanged
 
-            #self.stack unchanged
-
-      def choose () :
-            match self.term[-1].isnumeric() :
-                case True :
-                    match self.term[-1] :
-                        case 0 :
+    def choose (self) :
+        foo=self.term[-1]
+        foo=foo.isnumeric()
+        match foo :
+            case True :
+                match self.term[-1] :
+                    case 0 :
                         self.zero()
-                        
-                        case _ :
+                    case _ :
                         self.success()
-            
-                  case False :
-                      match self.term[ -2 ]
-                          case -1 :
-                              self.app()
-                        case _ :
-                            self.abstract()
-      def load () :
+                        
+            case False :
+                match self.term[ -2 ]
+                    case -1 :
+                        self.app()
+                    case _ :
+                        self.abstract()
+                        def load () :
 
             
                             
